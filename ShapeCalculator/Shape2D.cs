c303c56d7917c116096428c1da74785d02a51397
Evaniee <Evaniee7@outlook.com>
2024-10-09 13:50:10 +0100

    Created Shape2D

---
+++ ShapeCalculator/Shape2D.cs
@@ -0,0 +1,56 @@
+﻿namespace ShapeCalculator
+{
+    public abstract class Shape2D
+    {
+        protected double _width;
+        /// <summary>
+        /// Width of the 2D Shape
+        /// </summary>
+        public double Width
+        {
+            get
+            {
+                return _width;
+            }
+
+            private set
+            { 
+                if (value <= 0) throw new ArgumentException("Width must be positive.");
+                _width = value;
+            }
+        }
+        
+        protected double _height;
+        /// <summary>
+        /// Height of the 2D Shape
+        /// </summary>
+        public double Height
+        {
+            get
+            {
+                return _height;
+            }
+
+            private set
+            {
+                if (value <= 0) throw new ArgumentException("Height must be positive.");
+                _height = value;
+            }
+        }
+        
+        /// <summary>
+        /// Represents a 2D Shape
+        /// </summary>
+        /// <param name="width">Width of the 2D Shape</param>
+        /// <param name="height">Height of the 2D Shape</param>
+        /// <exception cref="ArgumentException"></exception>
+        public Shape2D(double width, double height)
+        {
+            if (width <= 0) throw new ArgumentException("Width must be positive.");
+            if (height <= 0) throw new ArgumentException("Height must be positive");
+
+            this._width = width;
+            this._height = height;
+        }
+    }
+}
