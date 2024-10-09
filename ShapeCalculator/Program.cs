7216e55f3ec8ac7e3fb73d3bb7ad2e38081f55d1
Evaniee <Evaniee7@outlook.com>
2024-10-09 13:32:13 +0100

    Add project files.

---
+++ ShapeCalculator/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ShapeCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
